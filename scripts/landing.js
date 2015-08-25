b79ca859302f9d20f79b40a807fc2c2696773a82
Sarah Binion <srhbinion@gmail.com>
2015-08-24 23:34:49 -0500

    Assignment 22 transitions and DOM selector

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,30 +1,16 @@
 var animatePoints = function() {
+    var points = document.getElementsByClassName('point');
+    
+    var transformValue = "scaleX(1) translateY(0)";
                 
-                var points = document.getElementsByClassName('point');
-                
-                var revealFirstPoint = function() {
-                    points[0].style.opacity = 1;
-                    points[0].style.transform = "scaleX(1) translateY(0)";
-                    points[0].style.msTransform = "scaleX(1) translateY(0)";
-                    points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-                };
-                
-                var revealSecondPoint = function() {
-                    points[1].style.opacity = 1;
-                    points[1].style.transform = "scaleX(1) translateY(0)";
-                    points[1].style.msTransform = "scaleX(1) translateY(0)";
-                    points[1].style.WebkitTransform = "scaleX(1) translateY(0)"; 
-                };
-                
-                var revealThirdPoint = function() {
-                    points[2].style.opacity = 1;
-                    points[2].style.transform = "scaleX(1) translateY(0)";
-                    points[2].style.msTransform = "scaleX(1) translateY(0)";
-                    points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-                };
-                
-                revealFirstPoint();
-                revealSecondPoint();
-                revealThirdPoint();
-                
-            };
+    var revealPoints = function() {
+        for (i = 0; points.length > i; i++) {
+            points[i].style.opacity = 1;
+            points[i].style.transform = transformValue
+            points[i].style.msTransform = transformValue
+            points[i].style.WebkitTransform = transformValue
+        };
+    };
+    
+    revealPoints();
+};
