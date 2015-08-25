be111727d84489fc40c35821bb6eb8254e12e44b
Sarah Binion <srhbinion@gmail.com>
2015-08-25 01:54:56 -0500

    Checkpoint 23, event listener

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,6 @@
-var animatePoints = function() {
-    var points = document.getElementsByClassName('point');
-    
+var pointsArray = document.getElementsByClassName('point');
+
+var animatePoints = function(points) {
     var transformValue = "scaleX(1) translateY(0)";
                 
     var revealPoints = function() {
@@ -13,4 +13,17 @@ var animatePoints = function() {
     };
     
     revealPoints();
+};
+
+window.onload = function() {
+    if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    }
+    
+    window.addEventListener('scroll', function(event) {
+        if (pointsArray[0].getBoundingClientRect().top <= 500) {
+            animatePoints(pointsArray);
+        }   
+    }); 
+    
 };
