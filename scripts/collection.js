7bbd7a1153731480eb582f2a39a5806beb8caa7d
Sarah Binion <srhbinion@gmail.com>
2015-08-28 16:53:37 -0500

    Checkpoint 24 collection view

---
+++ scripts/collection.js
@@ -0,0 +1,24 @@
+var collectionItemTemplate = 
+    +'<div class="collection-album-cotainer column fourth">'
+    +'   <img src="assets/images/album_covers/01.png"/>'
+    +'   <div class="collection-album-info caption">'
+    +'       <p>'
+    +'           <a class="album-name" href="/album.html"> The Colors </a>'
+    +'           <br/>'
+    +'           <a href="/album.html"> Pablo Picasso </a>'
+    +'           <br/>'
+    +'           X songs'
+    +'           <br/>'
+    +'       </p>'
+    +'   </dvi>'
+    +'</div>';
+
+window.onload = function() {
+    var collectionContainer = document.getElementsByClassName('album-covers')[0].childNodes[1];
+    
+    collectionContainer.innerHTML = '';
+    
+    for(var i = 0; i < 12; i++) {
+        collectionContainer.innerHTML += collectionItemTemplate;
+    }
+};
